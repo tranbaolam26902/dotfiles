9ae6f4c13fd6d185723ac1f359120ef7f4c60f7b
Campbells <tranbaolam26902@gmail.com>
2024-02-22 00:31:36 +0700

    Update plugins.lua

--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -1,5 +1,19 @@
 local plugins = {
     {
+        "nvim-tree/nvim-tree.lua",
+        cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+        init = function()
+            require("core.utils").load_mappings "nvimtree"
+        end,
+        opts = function()
+            return require "custom.configs.nvimtree"
+        end,
+        config = function(_, opts)
+            dofile(vim.g.base46_cache .. "nvimtree")
+            require("nvim-tree").setup(opts)
+        end,
+    },
+    {
         "neovim/nvim-lspconfig",
         dependencies = {
             "jose-elias-alvarez/null-ls.nvim",
