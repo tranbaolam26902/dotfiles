b443aa21ae5ba082c5d3b9ef413649bfcfaa8a23
Campbells <tranbaolam26902@gmail.com>
2024-02-22 13:32:44 +0700

    Create alias.cmd

---
+++ shell/cmd/alias.cmd
@@ -0,0 +1,10 @@
+@echo off
+doskey clear=cls
+doskey gs=git status
+doskey gaa=git add .
+doskey gp=git push
+doskey gpc=git push -u origin campbells
+doskey gpm=git push -u origin master
+doskey gplm=git pull origin master
+doskey gra=git restore .
+doskey dnr=dotnet run --urls=https://localhost:7272
