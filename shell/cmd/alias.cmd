c67938f03a531b6afd6771754fd6920c8753351c
Campbells <tranbaolam26902@gmail.com>
2024-02-26 07:55:40 +0700

    Update alias.cmd

--- shell/cmd/alias.cmd
+++ shell/cmd/alias.cmd
@@ -1,12 +1,21 @@
 @echo off
+
 doskey clear=cls
+
+doskey gi=git init
+doskey gbm=git branch -M master
 doskey gs=git status
+doskey gd=git diff
 doskey gaa=git add .
+doskey gcm=git commit '$*'
 doskey gp=git push
 doskey gpc=git push -u origin campbells
 doskey gpm=git push -u origin master
+doskey gpl=git pull
 doskey gplm=git pull origin master
+doskey gplm=git pull origin campbells
 doskey gra=git restore .
-doskey dnb=dotnet build
+
 doskey dndb=dotnet-ef Database Update
-doskey dnr=dotnet run --urls=https://localhost:7272
+doskey dnb=dotnet build
+doskey dnr=dotnet run --urls=https://localhost:$*
