b940e6f8c541dcdb5a484d7e5276fcbef9e23823
campbells <tranbaolam26902@gmail.com>
2024-08-21 14:46:36 +0700

    add tailwind-sorter plugin

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -3,4 +3,6 @@ require 'nvchad.mappings'
 local map = vim.keymap.set
 
 map('n', ';', ':', { desc = 'CMD enter command mode' })
-map('n', '<leader>lf', ':lua vim.diagnostic.open_float()<CR>')
+map('n', '<leader>lf', ':lua vim.diagnostic.open_float()<CR> <BAR> :TailwindSort<CR>')
+
+vim.cmd 'TailwindSortOnSaveToggle' -- Sort tailwind classes on save
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -59,4 +59,18 @@ return {
         lazy = false,
         event = { 'BufReadPre', 'BufNewFile' },
     },
+
+    {
+        'nvim-lua/plenary.nvim',
+        lazy = false,
+    },
+
+    {
+        'laytan/tailwind-sorter.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
+        on_save_pattern = { '*.html', '*.jsx', '*.tsx' },
+        lazy = false,
+        build = 'cd formatter && npm ci && npm run build',
+        config = true,
+    },
 }
