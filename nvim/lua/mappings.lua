6384d66c6fcc531759d0e0a28b4d6875773aa33c
campbells <tranbaolam26902@gmail.com>
2024-10-09 18:20:35 +0700

    add vertical split keymap

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,8 +1,10 @@
 require 'nvchad.mappings'
 
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 map('n', ';', ':', { desc = 'CMD enter command mode' })
-map('n', '<leader>lf', ':lua vim.diagnostic.open_float()<CR>')
+map('n', '<leader>lf', ':lua vim.diagnostic.open_float()<CR>', opts)
+map('n', 'sv', ':vsplit<CR> <BAR> :Telescope find_files<CR>', opts)
 
 vim.cmd 'TailwindSortOnSaveToggle' -- sort tailwind classes on save
