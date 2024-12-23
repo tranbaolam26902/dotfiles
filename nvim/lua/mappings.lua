b325e7e1a32f95684f257a06a097447bb08c53ed
campbells <tranbaolam26902@gmail.com>
2024-12-23 14:12:34 +0700

    update delete all buffers command

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,4 +6,4 @@ local opts = { noremap = true, silent = true }
 map('n', ';', ':', { desc = 'CMD enter command mode' })
 map('n', '<leader>lf', ':lua vim.diagnostic.open_float()<CR>', opts) -- shows diagnostic
 map('n', 'sv', ':vsplit<CR> <BAR> :Telescope find_files<CR>', opts) -- splits the window vertically and opens the file finder
-map('n', '<leader>X', ':%bd<CR>', opts) -- deletes all buffers
+map('n', '<leader>X', ':%bd!<CR>', opts) -- deletes all buffers
