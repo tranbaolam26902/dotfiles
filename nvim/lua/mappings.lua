347650000aad2e5c0c26f7e4a71a237ac73c5b45
campbells <tranbaolam26902@gmail.com>
2025-01-14 12:41:32 +0700

    update keymap

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,5 +7,6 @@ map('n', ';', ':', { desc = 'CMD enter command mode' })
 map('n', '<leader>lf', ':lua vim.diagnostic.open_float()<CR>', opts) -- shows diagnostic
 map('n', 'sv', ':vsplit<CR> <BAR> :Telescope find_files<CR>', opts) -- splits the window vertically and opens the file finder
 map('n', '<leader>X', ':%bd<CR>', opts) -- deletes all buffers
+map('n', '<leader>x', ':bd<CR>', opts) -- delete current buffer
 map('n', '<leader>cn', ':NvimTreeCollapse<CR>', opts) -- collapses NvimTree
 map('n', '<leader>gm', ':Gitsigns blame_line<CR>', opts) -- shows git commit message
