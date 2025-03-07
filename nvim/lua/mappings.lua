2188ea9e12c47011a854217947c538fc075921c0
campbells <tranbaolam26902@gmail.com>
2025-03-07 09:35:42 +0700

    add keymaps

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -5,8 +5,9 @@ local opts = { noremap = true, silent = true }
 
 map('n', ';', ':', { desc = 'CMD enter command mode' })
 map('n', '<leader>lf', ':lua vim.diagnostic.open_float()<CR>', opts) -- shows diagnostic
-map('n', 'sv', ':vsplit<CR> <BAR> :Telescope find_files<CR>', opts) -- splits the window vertically and opens the file finder
+map('n', 'sv', ':vsplit<CR>', opts) -- splits the window vertically
 map('n', '<leader>X', ':%bd<CR>', opts) -- deletes all buffers
 map('n', '<leader>x', ':bd<CR>', opts) -- delete current buffer
 map('n', '<leader>cn', ':NvimTreeCollapse<CR>', opts) -- collapses NvimTree
+map('n', '<leader>cc', '[m', opts) -- go to current context
 map('n', '<leader>gm', ':Gitsigns blame_line<CR>', opts) -- shows git commit message
