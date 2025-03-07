9a0ae414f8d9371c5f8a0e3ea23d4c4dd0de170f
campbells <tranbaolam26902@gmail.com>
2025-03-07 10:47:35 +0700

    add descriptions for key map

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -2,14 +2,17 @@ require 'nvchad.mappings'
 
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
+local function extend_opts(extra)
+    return setmetatable(extra or {}, { __index = opts })
+end
 
 map('n', ';', ':', { desc = 'CMD enter command mode' })
-map('n', '<leader>lf', ':lua vim.diagnostic.open_float()<CR>', opts) -- shows diagnostic
-map('n', 'sv', ':vsplit<CR>', opts) -- splits the window vertically
-map('n', '<leader>X', ':%bd<CR>', opts) -- deletes all buffers
-map('n', '<leader>x', ':bd<CR>', opts) -- delete current buffer
-map('n', '<leader>cn', ':NvimTreeCollapse<CR>', opts) -- collapses NvimTree
+map('n', '<leader>lf', ':lua vim.diagnostic.open_float()<CR>', extend_opts { desc = 'Show diagnostic' }) -- shows diagnostic
+map('n', 'sv', ':vsplit<CR>', extend_opts { desc = 'Vertical split' }) -- splits the window vertically
+map('n', '<leader>X', ':%bd<CR>', extend_opts { desc = 'Delete all buffers' }) -- deletes all buffers
+map('n', '<leader>x', ':bd<CR>', extend_opts { desc = 'Delete current buffer' }) -- delete current buffer
+map('n', '<leader>cn', ':NvimTreeCollapse<CR>', extend_opts { desc = 'Collapse NvimTree' }) -- collapses NvimTree
 vim.keymap.set('n', '<leader>cc', function()
     require('treesitter-context').go_to_context(vim.v.count1)
-end, { silent = true, desc = 'Jump to Treesitter Parent Context' }) -- jumps to context (upwards)
-map('n', '<leader>gm', ':Gitsigns blame_line<CR>', opts) -- shows git commit message
+end, { silent = true, desc = 'Jump to Context' }) -- jumps to context (upwards)
+map('n', '<leader>gm', ':Gitsigns blame_line<CR>', extend_opts { desc = 'Show commit message' }) -- shows git commit message
