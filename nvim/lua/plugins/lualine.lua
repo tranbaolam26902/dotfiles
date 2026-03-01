ccff5173199e2773e5511460c0319475edf740e9
campbells <tranbaolam26902@gmail.com>
2026-03-01 21:29:14 +0700

    chore: remove unused sections in lualine

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    sections = {
+      lualine_c = { "filename" },
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { "location" },
+    },
+  },
+}
