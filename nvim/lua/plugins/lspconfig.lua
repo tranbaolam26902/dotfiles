05fd4b0e324406e42273809c05192559b85da244
campbells <tranbaolam26902@gmail.com>
2026-03-05 12:08:24 +0700

    fix(lsp): disable warning at unknown tailwind rules

---
+++ nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,26 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      cssls = {
+        settings = {
+          css = {
+            lint = {
+              unknownAtRules = "ignore",
+            },
+          },
+          scss = {
+            lint = {
+              unknownAtRules = "ignore",
+            },
+          },
+          less = {
+            lint = {
+              unknownAtRules = "ignore",
+            },
+          },
+        },
+      },
+    },
+  },
+}
